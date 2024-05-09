8c736e804e51c5055bb62af0bb0c544c62951d21
woo-dayoung <daisy123659@gmail.com>
2024-05-09 18:44:11 +0900

    MainController::index 메소드 추가

---
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.exam.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/sbb")
+    public void index(){
+        System.out.println("첫 시작");
+    }
+}
