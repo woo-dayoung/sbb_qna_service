4e295729af9de15982490fab2298198df8d2dd10
woo-dayoung <daisy123659@gmail.com>
2024-05-10 00:22:15 +0900

    livereload 적용

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     @RequestMapping("/sbb")
+    // 아래 함수의 리턴값을 그대로 브라우저에 표시
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
     @ResponseBody
     public String index(){
-        return "안녕하세요";
+        return "안녕하세요.akjhsbdf";
     }
 }
