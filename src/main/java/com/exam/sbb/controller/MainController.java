d9523148b1b75fd7c7bcc28275dfb681778bc1ca
woo-dayoung <daisy123659@gmail.com>
2024-05-14 03:38:51 +0900

    구구단 구현

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -3,6 +3,9 @@ package com.exam.sbb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class MainController {
     private int increaseNo = -1;
@@ -60,5 +63,20 @@ public class MainController {
     }
 
 
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String showGugudan(Integer dan,Integer limit){
+        if(dan == null){
+            dan = 9;
+        }
+        if(limit == null){
+            limit = 9;
+        }
+        final Integer finalDan = dan;
+        return IntStream.rangeClosed(1,limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(finalDan,i,finalDan * i))
+                .collect(Collectors.joining("<br>\n"));
+    }
 
 }
