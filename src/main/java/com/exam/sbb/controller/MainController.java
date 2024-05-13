0ecb3399835cfd2bdb3ecb34f982a9e11218b724
woo-dayoung <daisy123659@gmail.com>
2024-05-13 23:58:19 +0900

    plus,minus,increase

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
+    private int increaseNo = -1;
     @RequestMapping("/sbb")
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
@@ -40,4 +41,24 @@ public class MainController {
                 <h1>안녕하세요. GET 방식으로 오신걸 환영합니다.</h1>
                 """.formatted(age);
     }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a,int b){
+        return a + b;
+    }
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a,int b){
+        return a - b;
+    }
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease(){
+       increaseNo++;
+        return increaseNo;
+    }
+
+
+
 }
